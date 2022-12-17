87ffc08607131b725aff3df804024e01dbb1a932
unknown <harishjeeva71@gmail.com>
2022-12-17 22:15:35 +0530

    First Commit

---
+++ src/main/java/com/example/supplychainhari17dec/HelloApplication.java
@@ -0,0 +1,45 @@
+package com.example.supplychainhari17dec;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    private Pane createContent(){
+
+        Pane root = new Pane();
+        return root;
+    }
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(createContent());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychainhari17dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychainhari17dec to javafx.fxml;
+    exports com.example.supplychainhari17dec;
+}
